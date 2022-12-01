436d4e81071789c58654cb236251ec634212f2d7
Ultimate0266 <111071951+Ultimate0266@users.noreply.github.com>
2022-12-01 20:28:09 +0100

    import modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,1 +1,13 @@
-sbsbsn
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    from add_0 import add
+
+    a = 1
+    b = 2
+
+    a = int(a)
+    b = int(b)
+
+    print("{} + {} = {}".format(a, b, add(a, b)))

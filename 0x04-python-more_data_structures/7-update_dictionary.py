2dd582b02138ff22c1d75909d3a4faa26f1521d7
Ultimate0266 <111071951+Ultimate0266@users.noreply.github.com>
2022-12-08 00:24:34 +0100

    More data structures

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,1 +1,5 @@
-ifdhh
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """ replaces or adds key/value in a dictionary."""
+    a_dictionary[key] = value
+    return a_dictionary

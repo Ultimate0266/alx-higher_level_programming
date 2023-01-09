b92ef58738754f128d7a296e6e82257db88b0a57
Ultimate0266 <111071951+Ultimate0266@users.noreply.github.com>
2023-01-09 21:04:20 +0100

    python inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -7,13 +7,4 @@ def lookup(obj):
     ''' function: lookup()
     Returns a list object
     '''
-    return dir(obj)#!/usr/bin/python3
-''' function that returns the list
-'''
-
-
-def lookup(obj):
-    ''' function: lookup()
-    Returns a list object
-    '''
     return dir(obj)

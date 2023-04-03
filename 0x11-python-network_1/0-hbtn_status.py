f34ab0b15f77fdf1aae75490a12f48932a3953d8
Saefullah Sadiq <sadiqsaefullah@gmail.com>
2023-04-03 16:04:40 -0700

    python network 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+"""A script that
+- fetches https://intranet.hbtn.io/status.
+- uses urlib package
+"""
 
-"""This is a Python script that fetches the status of ' https://alx-intranet.hbtn.io/status'"""
 
+if __name__ == '__main__':
+    import urllib.request
 
-import urllib.request
-
-if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+        content = res.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
